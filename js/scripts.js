f07f9e08ed19cc234096344e60a5ac2d46163f7e
TichiOigara <sheilanyatichi@gmail.com>
2019-03-06 14:57:37 +0300

    add content to missing-people.html, style it and add button functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,46 @@
 //     Author     : Peter Wachira , Yasmin , Sheila Oigara , Mohammed Mwai
 //
 //  -->
+$(document).ready(function(){
+  // $(".missingBtn").first().click(function(){
+  //   $(".personDetails").toggle();
+  // });
+  $("#missingBtn1").click(function(){
+    $("#personDetails1").toggle();
+  });
+  $("#missingBtn2").click(function(){
+    $("#personDetails2").toggle();
+  });
+  $("#missingBtn3").click(function(){
+    $("#personDetails3").toggle();
+  });
+  $("#missingBtn4").click(function(){
+    $("#personDetails4").toggle();
+  });
+  $("#missingBtn5").click(function(){
+    $("#personDetails5").toggle();
+  });
+  $("#missingBtn6").click(function(){
+    $("#personDetails6").toggle();
+  });
+  $("#foundBtn1").click(function(){
+    $("#foundDetails1").toggle();
+  });
+  $("#foundBtn2").click(function(){
+    $("#foundDetails2").toggle();
+  });
+  $("#foundBtn3").click(function(){
+    $("#foundDetails3").toggle();
+  });
+  $("#foundBtn4").click(function(){
+    $("#foundDetails4").toggle();
+  });
+  $("#foundBtn5").click(function(){
+    $("#foundDetails5").toggle();
+  });
+  $("#foundBtn6").click(function(){
+    $("#foundDetails6").toggle();
+  });
+
+
+});

1ef04d127b03f8257019374728c2506839bebcca
TichiOigara <sheilanyatichi@gmail.com>
2019-03-07 08:46:02 +0300

    add search functionality using js/scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,49 @@
 //     Author     : Peter Wachira , Yasmin , Sheila Oigara , Mohammed Mwai
 //
 //  -->
+
+function searchName(name){
+  var nameLower = name.toLowerCase();
+  var missingPersons = ["jane", "doe", "john", "dow", "jenny", "pow", "johnny", "test", "joe", "mel", "jem", "allie"];
+
+  var result;
+  for(var i =0; i<missingPersons.length; i++){
+    if(nameLower === missingPersons[i]){
+      result = missingPersons.indexOf(missingPersons[i]);
+      break;
+    }else{
+      result= NaN;
+    }
+  }
+  return result;
+}
+function checkID(result){
+    var personID =["personID1","personID1","personID2","personID2","personID3","personID3","personID4","personID4","personID5","personID5","personID6","personID6"];
+    var elementID ;
+    for(var i =0; i<personID.length; i++){
+      if(result == personID.indexOf(personID[i])){
+        elementID = personID[i] ;
+        break;
+      }else {
+        elementID = "Not Present";
+      }
+    }
+return elementID;
+}
+function showResults(elementID){
+  if (elementID === "Not Present"){
+    $(".missingElements").hide();
+    $("resetBtn")
+    $("#displayResults").text("No match was found!")
+  }else{
+    $(".missingElements").hide();
+    $("#"+elementID).show();
+  }
+}
+
+
 $(document).ready(function(){
-  // $(".missingBtn").first().click(function(){
-  //   $(".personDetails").toggle();
-  // });
+
   $("#missingBtn1").click(function(){
     $("#personDetails1").toggle();
   });
@@ -44,6 +83,14 @@ $(document).ready(function(){
   $("#foundBtn6").click(function(){
     $("#foundDetails6").toggle();
   });
-
+  $("#missingForm").submit(function(event){
+    event.preventDefault();
+    var firstName = $("input#missingName").val();
+    //alert(firstName);
+    var checkName = searchName(firstName);
+    var elementName = checkID(checkName);
+    alert(elementName);
+    showResults(elementName);
+  });
 
 });
